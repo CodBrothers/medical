ff5f8a5e8e40a7f2f6af09215e7f07237790f3a2
CodBrothers <codeerssr@gmail.com>
2024-06-05 21:50:25 +0530

    npm i some pkges

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
         <nav className="bg-blue-500 p-4">
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white text-2xl font-bold">
-                    <Link to="/">Medical Bros</Link>
+                    <Link to="/home">Medical Bros</Link>
                 </div>
                 <div className="flex space-x-4 ">
                     <Link to="/home" className="text-white hover:scale-125 transform transition-transform duration-300 px-3">Home</Link>
